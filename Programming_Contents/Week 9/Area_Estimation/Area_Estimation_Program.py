801d762fc316dae3112af1cc1e1d01f7f1e0b12c
Kasper-GitHub <81840191+Kasper-GitHub@users.noreply.github.com>
2025-03-15 18:27:53 +0530

    New Project

--- Programming_Contents/Week 9/Area_Estimation/Area_Estimation_Program.py
+++ Programming_Contents/Week 9/Area_Estimation/Area_Estimation_Program.py
@@ -1,5 +1,26 @@
+import scipy
 from PIL import Image
+import numpy
+import random
+import imageio.v2 as imageio
 
-im = Image.open('test.jpg')
-rgb_im = im.convert('RGB')
-r,g,b = rgb_im.getpixel((1,1))
+img = imageio.imread("Random_Map.png")
+count_orange = 0
+count_black = 0
+count = 0
+
+while count < 10000:
+    x = random.randint(0,580)
+    y = random.randint(0,580)
+    z = 0
+
+    if img[x][y][z] > 225:
+        count_orange += 1
+        count += 1
+    elif img[x][y][z] > 180:
+        count_black += 1
+        count += 1
+    else:
+        count += 1
+
+print("Orange Area : ",count_orange, "\nBlack Area :", count_black, "\nUnconsidered Area :", count-count_black-count_orange)
