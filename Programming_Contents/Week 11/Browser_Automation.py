793963ea145513175026b6dc3c48886f51f680aa
Kasper-GitHub <81840191+Kasper-GitHub@users.noreply.github.com>
2025-04-05 21:32:58 +0530

    New Assignment

--- Programming_Contents/Week 11/Browser_Automation.py
+++ Programming_Contents/Week 11/Browser_Automation.py
@@ -1,3 +1,12 @@
 from selenium import webdriver
 
-browser = webdriver.Chrome(r'C:\Users\ankus\Downloads\chrome-win64')
+def get_default_edge_options():
+    options = webdriver.EdgeOptions()
+    options.add_argument("--no-sandbox")
+    options.add_experimental_option("detach", True)
+    return options
+
+options = get_default_edge_options()
+browser = webdriver.Edge(options=options)
+
+browser.get("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php")
